62939a916c26d50dd3522a3ae442c14f9b7d40da
Summer Davis <summerd@tlgcohort.com>
2024-07-17 15:21:45 -0500

    code modularized

---
+++ js-modules/card.js
@@ -0,0 +1,34 @@
+export function createCard({
+  photoURL,
+  destinationName,
+  location,
+  description,
+}) {
+  let newCard = document.createElement("div");
+  newCard.className = "card";
+  newCard.innerHTML = `
+      <img class="card-img-top" src=${photoURL} alt="Card image cap"/>
+      <div class="card-body">
+        <h5 class="card-destination">${destinationName}</h5>
+        <h6 class="card-location">${location}</h6>
+        <p class="card-description">${description}</p>
+        <div class="card-buttons row">
+          <span class="col-md-6" id="edit-btn"><button class="btn btn-warning">Edit</button></span>
+          <span class="col-md-6" id="remove-btn"><button class="btn btn-danger">Remove</button></span>
+        </div>
+      </div>
+      `;
+  return newCard;
+}
+
+export function alterHeading() {
+  document.querySelector("h4").textContent = "My WishList";
+}
+
+export function appendCard(newCard) {
+  document.getElementById("cardContainer").appendChild(newCard);
+}
+
+export function clearForm() {
+  document.querySelector("form").reset();
+}
